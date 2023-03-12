3e5118ac4d1236e96c7c3c5572ca80152993225d
shaghaygh <shqayqm145@gmail.com>
2023-03-12 20:13:14 +0330

    12/3/2023 , 20:00

--- all.js
+++ all.js
@@ -1,4 +1,50 @@
-// var search = document.querySelector("input[type = search]");
-// function func(){
-//     search.style.border = "none";
-// }
+$ (function() {
+    $(".img1").show();
+    $(".img2").hide();
+    $(".img3").hide();
+    $(".img4").hide();
+    $(".btnitem1").addClass('active');
+    $(".btnitem2").removeClass('active');
+    $(".btnitem3").removeClass('active');
+    $(".btnitem4").removeClass('active');
+    $(".btnitem1").click(function () { 
+        $(".img1").show();
+        $(".img2").hide();
+        $(".img3").hide();
+        $(".img4").hide();
+        $(".btnitem1").addClass('active');
+        $(".btnitem2").removeClass('active');
+        $(".btnitem3").removeClass('active');
+        $(".btnitem4").removeClass('active');
+    });
+    $(".btnitem2").click(function () { 
+        $(".img1").hide();
+        $(".img2").show();
+        $(".img3").hide();
+        $(".img4").hide();
+        $(".btnitem1").removeClass('active');
+        $(".btnitem2").addClass('active');
+        $(".btnitem3").removeClass('active');
+        $(".btnitem4").removeClass('active');
+    });
+    $(".btnitem3").click(function () { 
+        $(".img1").hide();
+        $(".img2").hide();
+        $(".img3").show();
+        $(".img4").hide();
+        $(".btnitem1").removeClass('active');
+        $(".btnitem2").removeClass('active');
+        $(".btnitem3").addClass('active');
+        $(".btnitem4").removeClass('active');
+    });
+    $(".btnitem4").click(function () { 
+        $(".img1").hide();
+        $(".img2").hide();
+        $(".img3").hide();
+        $(".img4").show();
+        $(".btnitem1").removeClass('active');
+        $(".btnitem2").removeClass('active');
+        $(".btnitem3").removeClass('active');
+        $(".btnitem4").addClass('active');
+    });
+})
